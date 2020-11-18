e0f5b59d65e85e4f2bb6c00cf02bf46ae01390bf
Jace Laquerre <jace.laquerre@uvm.edu>
2020-11-17 20:43:57 -0500

    testing menu format

--- footer.php
+++ footer.php
@@ -2,6 +2,4 @@
 <footer>
     <a href="../index.php">Return Home</a>
     <p style="text-indent: 0">Created by: Matt Zahar & Jace Laquerre. Contact site master: <a href="mailto:mzahar.uvm.edu">mzahar.uvm.edu</a>.</p>
-    <p style="text-indent: 0">Contact information: <a href="mailto:mzahar.uvm.edu">mzahar.uvm.edu</a>.</p>
-    <p style="text-indent: 0">Contact information: <a href="mailto:jlaquerr.uvm.edu">jace.laquerre.uvm.edu</a>.</p>
 </footer>
--- menu.php
+++ menu.php
@@ -1,7 +1,28 @@
 <?php
 include ("top.php");
 ?>
+<main>
+    <h1>Menu</h1>
 
+    <h2>Sandwiches</h2>
+
+    <h3><span>Country Sandwich</span> $5.99<h3>
+            <p>
+                Lettuce, tomato etc
+            </p>
+
+            <h3><span>Turkey Club</span> $4.99</h3>
+            <p>
+                turkey, bacon, tomato and lettuce on toast with mayonnaise
+            </p>
+
+            <h2>Fajitas & Quesadillas</h2>
+
+            <h3><span>Bacon Quesadilla</span> $6.99</h3>
+            <p>
+                smoked bacon and cheese etc&#8230;
+            </p>
+</main>
 <?php
 include ("footer.php");
 ?>
