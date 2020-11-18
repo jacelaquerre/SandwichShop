877fb9788fda56e250b5404a685b4edbcc651ffb
Jace Laquerre <jace.laquerre@uvm.edu>
2020-11-17 20:52:56 -0500

    start on orderlookup

--- menu.php
+++ menu.php
@@ -14,21 +14,12 @@ include ("top.php");
 
     foreach ($sandwiches as $sandwich) {
         $english_format_money = "$" . number_format($sandwich["Price"], 2, '.', ',');
-        print '<h3><span>' . $sandwich["Sandwich_Name"] . '</span>' . $english_format_money . '<h3>';
+        print '<h3><span>' . $sandwich["Sandwich_Name"] . '</span>' . ' - ' . $english_format_money . '<h3>';
         print '<p>' . $sandwich["Description"] . '</p>';
     }
     ?>
-    <h3><span>Country Sandwich</span> $5.99<h3>
-    <p>Lettuce, tomato etc</p>
-
-    <h3><span>Turkey Club</span> $4.99</h3>
-    <p>turkey, bacon, tomato and lettuce on toast with mayonnaise</p>
-
-    <h2>Fajitas & Quesadillas</h2>
-
-    <h3><span>Bacon Quesadilla</span> $6.99</h3>
-    <p>smoked bacon and cheese etc&#8230;</p>
 </main>
+
 <?php
 include ("footer.php");
 ?>
--- updateOrder.php
+++ updateOrder.php
@@ -1,7 +1,17 @@
 <?php
 include ("top.php");
 ?>
+<main>
+    <form action = "<?php print PHP_SELF; ?>"
+          id="frmOption"
+          method = "post">
+        <fieldset class="contact">
+            <label for="name">Order #</label>
+            <input type="text" id="order" name="order">
+        </fieldset>    
+    </form>
 
+</main>
 <?php
 include ("footer.php");
 ?>
